dfacb49c959bf439ae9f900c49f7089365bc5bde
xiang-k <59462278+xiang-k@users.noreply.github.com>
2023-05-11 17:20:41 +0800

    Update png2fig.m

--- png2fig.m
+++ png2fig.m
@@ -1,11 +1,23 @@
 
 % saveas(gcf, ['./temp/image' num2str(i) '.png']);
 
-A = imread(['./temp/lx/' num2str(1) '.png']);
+pngPath = '../result/fu_fig/';
+figName = [pngPath 'fu_fig.gif'];
+DelayTime = 0.1;
+
+includeFile = dir(pngPath);
+pngsName = string({includeFile.name})';
+pngsName = pngsName(3:end,:);
+% pngsName_s = sort(pngsName);
+% A = imread([pngPath pngsName(1,:)]);
+
+
+A = imread([pngPath num2str(0) '.png']);
 [I,map]=rgb2ind(A,256);
-imwrite(I,map,'bonuli.gif','DelayTime',1,'LoopCount',Inf);
-for i=2:1:34
-    A = imread(['./temp/lx/' num2str(i) '.png']);
+imwrite(I,map,figName,'DelayTime',1,'LoopCount',Inf);
+
+for i=1:1:50
+    A = imread([pngPath num2str(i) '.png']);
     [I,map]=rgb2ind(A,256);
-    imwrite(I,map,'bonuli.gif','WriteMode','append','DelayTime',0.1);
+    imwrite(I,map,figName,'WriteMode','append','DelayTime',DelayTime);
 end
